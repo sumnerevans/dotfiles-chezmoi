11867c57ec53323bdde076e203dc9322835b1eb8
Sumner Evans <me@sumnerevans.com>
2020-05-30 02:20:33 -0600

    Both monitors horizontal

--- bin/executable_display-configuration.sh
+++ bin/executable_display-configuration.sh
@@ -2,6 +2,11 @@
 # Figures out how to XRandr properly for whatever the current display
 # configuration is.
 
+set -x
+
+# ROTATION="left"
+ROTATION="normal"
+
 DP1=$(xrandr | grep '^DP1 connected')
 DP2=$(xrandr | grep '^DP2 connected')
 DP23=$(xrandr | grep '^DP2-3 connected')
@@ -14,23 +19,23 @@ DP4=$(xrandr | grep 'DP-4 connected')
 
 if [[ $DP1 != "" ]]; then
     # External monitor at home
-    xrandr --output DP1 --mode 1920x1200 --right-of eDP1 --rotate left
+    xrandr --output DP1 --mode 1920x1200 --right-of eDP1 --rotate $ROTATION
 elif [[ $DP2 != "" ]]; then
     # External monitor at home (other USB-C port)
-    xrandr --output DP2 --mode 1920x1200 --right-of eDP1 --rotate left
+    xrandr --output DP2 --mode 1920x1200 --right-of eDP1 --rotate $ROTATION
 elif [[ $DP4 != "" ]]; then
     # Double monitor set up with Desktop
     xrandr --output DP-0 --mode 2560x1440 --rate 144.00
-    xrandr --output DP-4 --mode 1920x1200 --right-of DP-0 --rotate left
+    xrandr --output DP-4 --mode 1920x1200 --right-of DP-0 --rotate $ROTATION
 elif [[ $DP23 != "" ]]; then
     # External monitor at home (USB-C dock)
-    xrandr --output DP2-3 --mode 1920x1200 --right-of eDP1 --rotate left
+    xrandr --output DP2-3 --mode 1920x1200 --right-of eDP1 --rotate $ROTATION
 elif [[ $DP13 != "" ]]; then
     # External monitor at home (USB-C dock)
-    xrandr --output DP1-3 --mode 1920x1200 --right-of eDP1 --rotate left
+    xrandr --output DP1-3 --mode 1920x1200 --right-of eDP1 --rotate $ROTATION
 elif [[ $DP3 != "" ]]; then
     # External monitor at home, from MacBook Pro (make sure to scale).
-    xrandr --output DP-3 --mode 1920x1200 --right-of DP-2 --scale 1.5x1.5 --rotate left
+    xrandr --output DP-3 --mode 1920x1200 --right-of DP-2 --scale 1.5x1.5 --rotate $ROTATION
 elif [[ $HDMI2 != "" ]]; then
     # External monitor at home, from MacBook Pro (make sure to scale).
     xrandr --output HDMI2 --mode 1920x1200 --right-of eDP1
