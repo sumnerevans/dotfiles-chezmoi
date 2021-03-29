2b107f1872626901a2ab2e832bd9046edb3abc20
Sumner Evans <me@sumnerevans.com>
2021-03-29 15:16:37 -0600

    Remove tons of unnecessary configs and moved some to home manager

--- bin/executable_offlineimap-helper.py
+++
@@ -1,7 +0,0 @@
-#! /usr/bin/env python3
-from subprocess import check_output
-
-
-def get_pass(account):
-    """Get a Mail/* password from the 'pass' password manager."""
-    return check_output('pass Mail/' + account, shell=True).splitlines()[0]
